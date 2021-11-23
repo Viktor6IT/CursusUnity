44421595a04b1bdd821d4152955b2a0907fe9a75
Viktor6IT <93328625+Viktor6IT@users.noreply.github.com>
2021-11-23 15:29:32 +0100

    unit 3.3

--- prototype(3)/Assets/Scripts/PlayerController.cs
+++ prototype(3)/Assets/Scripts/PlayerController.cs
@@ -5,25 +5,28 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody PlayerRB;
-    private float jumpPower = 10.0f;
-    private float gravityModifier = 1.5f;
+    private float jumpPower = 650.0f;
+    private float gravityModifier = 1.8f;
     public bool IsOnGround = true;
     public bool Gameover;
+    private Animator PlayerAnimation;
 
     // Start is called before the first frame update
     void Start()
     {
         PlayerRB = GetComponent<Rigidbody>();
+        PlayerAnimation = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && IsOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && IsOnGround && !Gameover)
         {
-            PlayerRB.AddForce(Vector3.up * jumpPower,ForceMode.Impulse);
+            PlayerRB.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
             IsOnGround = false;
+            PlayerAnimation.SetTrigger("Jump_trig");
         }
     }
     private void OnCollisionEnter(Collision collision)
@@ -36,6 +39,8 @@ public class PlayerController : MonoBehaviour
         {
             Gameover = true;
             Debug.Log("Game is over");
+            PlayerAnimation.SetBool("Death_b", true);
+            PlayerAnimation.SetInteger("DeathType_int", 1);
         }
     }
 }
